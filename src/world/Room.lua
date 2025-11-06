6593a004bd81f16a38e6100d818446da1b7537b4
nicopico250 <preciadon26@student.jhs.net>
2025-11-06 13:37:29 -0800

    change

--- src/world/Room.lua
+++ src/world/Room.lua
@@ -195,7 +195,7 @@ function Room:update(dt)
             self.player:goInvulnerable(1.5)
 
             if self.player.health == 0 then
-                gStateMachine:change('game-over')
+                gStateMachine:change('game-over', self.player.roomCounter)
             end
         end
     end
